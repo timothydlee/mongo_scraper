9b0178a40907f6252ced05df6d0ef72796c93a8d
Timothy Lee <timothy.d.lee@live.com>
2017-01-13 23:25:36 -0600

    changed app.listen to include process.env.PORT

--- server.js
+++ server.js
@@ -153,6 +153,6 @@ app.get('/articles', (req, res) => {
 
 
 // Listen on port 3000
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
 	console.log("App running on port 3000!");
 });
