2900049b927de1148f2b732a1853c84c91e60bae
Timothy Lee <timothy.d.lee@live.com>
2017-01-29 20:34:57 -0600

    added a save article feature

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-var express 	= require('express');
-var exphbs 		= require('express-handlebars');
+var express 		= require('express');
+var exphbs 			= require('express-handlebars');
 var bodyParser 	= require('body-parser');
-var mongoose 	= require('mongoose');
-var request 	= require('request');
-var cheerio 	= require('cheerio');
+var mongoose 		= require('mongoose');
+var request 		= require('request');
+var cheerio 		= require('cheerio');
 
-var Article 	= require('./models/Article.js');
-var Comment 	= require('./models/Comment.js');
+var Article 		= require('./models/Article.js');
+var Comment 		= require('./models/Comment.js');
 
 var app = express();
 
@@ -15,6 +15,10 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static('public'));
 
+//Connection for dev purposes
+// mongoose.connect('mongodb://localhost/newsscraper')
+
+//Connect when ready to deploy to Heroku
 mongoose.connect('mongodb://heroku_wz6l96kq:ubnrroqm6j2ucpgvekid0lp7ej@ds117829.mlab.com:17829/heroku_wz6l96kq');
 var db = mongoose.connection;
 db.on('error', function (err) {
@@ -105,11 +109,11 @@ app.post('/articles/:id', function (req, res) {
 	});
 });
 
-app.post('/articles/:aId/delete/:cId', function (req, res) {
+app.post('/articles/:aId/delete/:cId', (req, res) => {
 	var articleId = req.params.aId;
 	var commentId = req.params.cId;
 
-	Article.update({'_id': articleId}, {$pull: {'comments': commentId}}, {'multi': false}, function (err, res) {
+	Article.update({'_id': articleId}, {$pull: {'comments': commentId}}, {'multi': false}, (err, res) => {
 		if (err) {
 			console.log(err);
 		} else {
@@ -126,16 +130,6 @@ app.post('/articles/:aId/delete/:cId', function (req, res) {
 	res.redirect('/articles/' + articleId);
 });
 
-// app.post('/articles/:aId/save', function (req, res) {
-// 	Article.update({ '_id': req.params.aId }, function (err, res) {
-// 		if (err) {
-// 			console.log(err);
-// 		} else {
-// 			console.log('Successfully saved article');
-// 		}
-// 	})
-// });
-
 app.get('/saved', (req, res) => {
 	//Finds articles where saved is true
 	Article.find({ 'saved' : true }, (err, doc) => {
@@ -151,16 +145,22 @@ app.get('/saved', (req, res) => {
 
 //Setting route to update an article to saved = true if user clicks "save article"
 app.post('/saved/:id', (req, res) => {
-	Article.update({ '_id' : req.params.id }, { $set : { 'saved' : true }}, (err, res) => 
-		//Console.logs error if any, otherwise console.logs res
-		(err) ? console.log(err) : console.log(res));
+	Article.update({ '_id' : req.params.id }, { $set : { 'saved' : true }}, (err, res) => {
+		if (err) {
+			console.log(err);
+		} else {
+			console.log(res);
+		}
+	});
+	res.redirect('/articles');
 })
 
 //Unsaves article
 app.post('/unsaved/:id', (req, res) => {
 	Article.update( { '_id' : req.params.id }, { $set : { 'saved' : false }}, (err, res) => 
-		(err) ? console.log(err) : console.log(res))
-})
+		(err) ? console.log(err) : console.log(res));
+	res.redirect('/saved');
+});
 
 //Listening to the port 3000 or the environment PORT
 app.listen(process.env.PORT || 3000, function () {
