b0ead194ada099d633ec77ea5ef2340fb763257a
Timothy Lee <timothy.d.lee@live.com>
2017-01-29 22:29:59 -0600

    added style to the app

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
+//requiring dependencies
 var express 		= require('express');
 var exphbs 			= require('express-handlebars');
 var bodyParser 	= require('body-parser');
 var mongoose 		= require('mongoose');
 var request 		= require('request');
 var cheerio 		= require('cheerio');
-
+//Incorporating models
 var Article 		= require('./models/Article.js');
 var Comment 		= require('./models/Comment.js');
 
+//Assigning var app to express();
 var app = express();
 
 app.use(bodyParser.urlencoded({
@@ -25,6 +27,7 @@ db.on('error', function (err) {
 	console.log('Mongoose Error: ', err);
 });
 
+//Setting handlebars as view engine
 app.engine('handlebars', exphbs({
 	defaultLayout: 'main'
 }));
@@ -35,73 +38,94 @@ app.get('/', function (req, res) {
 	res.redirect('/scraping');
 });
 
-app.get('/scraping', function (req, res) {
-	request.get('https://news.ycombinator.com/', function (err, request, body) {
+//Scraping site news.ycombinator.com/
+app.get('/scraping', (req, res) => {
+	request.get('https://news.ycombinator.com/', (err, request, body) => {
 		var $ = cheerio.load(body);
 
-		$('.title a[href^="http"], a[href^="https"]').each(function (index, element) {
+		//For each title class with a href http or https
+		$('.title a[href^="http"], a[href^="https"]').each((index, element) => {
 			var result = {};
 
+			//title and link data saved in the object "result"
 			result.title = $(element)[0].children[0].data
 			result.link = $(element)[0].attribs.href;
 
+			//creating new instance of Article
 			var entry = new Article(result);
-
-			entry.save(function (err, doc) {
+			//saving article to MongoDB
+			entry.save((err, doc) => {
+				//Messages for dev purposes
 				if (err) {
-					console.log('Article already in database');
+					console.log('Already scraped');
 				} else {
-					console.log('Finished scraping');
+					console.log('New article scraped');
 				}
 			})
 		});
 	});
+	//Redirecting to /articles route
 	res.redirect('/articles');
 });
 
-app.get('/articles', function (req, res) {
-	Article.find({}, function (err, doc) {
+//Default route that gets all the articles in the database
+app.get('/articles', (req, res) => {
+	Article.find({}, (err, doc) => {
+		//Displays error if any
 		if (err) {
 			console.log(err);
 		}
+		//Otherwise, renders articles handlebar template
 		else {
 			res.render('articles', {
+				//"articles" is the variable that will get looped over in articles.handlebars
 				articles: doc
 			});
 		}
 	});
 });
 
-app.get('/articles/:id', function (req, res) {
+//Creates get route to update articles with id parameter :id
+app.get('/articles/:id', (req, res) => {
+	//Mongoose to find the article with the id as set forth in the route
 	Article.findOne({'_id': req.params.id})
+		//Populates the comments
 		.populate('comments')
-		.exec(function (err, doc) {
+		//Executes the query
+		.exec((err, doc) => {
+			//consoles error if any
 			if (err) {
 				console.log(err);
 			}
+			//Else renders comments handlebars
 			else {
-				// console.log(doc);
 				res.render('comments', {
+					//Utilizes article as the variable that is looped over each instance of comments for a particular article to display all comments
 					article: doc
 				});
 			}
-		});
+	});
 });
 
-app.post('/articles/:id', function (req, res) {
-	var comment = req.body;
-	var newComment = new Comment(comment);
+//Post route for posting comments
+app.post('/articles/:id', (req, res) => {
+	//Creates new instance of Comment model with the req.body as the body of it
+	var newComment = new Comment(req.body);
 
-	newComment.save(function (err, doc) {
+	//Saves new comment via Mongoose
+	newComment.save((err, doc) => {
+		//If error, console log error
 		if (err) {
 			console.log(err);
+			//Else updates article with id :id to include a new comment with same id
 		} else {
 			var articleId = req.params.id;
 			Article.findOneAndUpdate({'_id': articleId}, {$push: {'comments': doc._id}})
-				.exec(function (err, doc) {
+				.exec((err, doc) => {
 					if (err) {
 						console.log(err);
 					} else {
+						//Redirects to the specific article's comment page
 						res.redirect('/articles/' + articleId);
 					}
 				});
@@ -109,19 +133,22 @@ app.post('/articles/:id', function (req, res) {
 	});
 });
 
+//Post route to delete a comment
 app.post('/articles/:aId/delete/:cId', (req, res) => {
 	var articleId = req.params.aId;
 	var commentId = req.params.cId;
-
+	//Update method in mongoose that searches for the id in the url, pulls the comments with the comment id in the url
 	Article.update({'_id': articleId}, {$pull: {'comments': commentId}}, {'multi': false}, (err, res) => {
+		//consoles error
 		if (err) {
 			console.log(err);
+			//else removes comment of that id
 		} else {
-			Comment.remove({'_id': commentId}, function (err, res) {
+			Comment.remove({'_id': commentId}, (err, res) => {
 				if(err) {
 					console.log(err);
 				} else {
-					console.log('Successful deletion of comment');
+					console.log('Comment deleted');
 				}
 			});
 		}
@@ -130,12 +157,14 @@ app.post('/articles/:aId/delete/:cId', (req, res) => {
 	res.redirect('/articles/' + articleId);
 });
 
+//Get route to display saved articles
 app.get('/saved', (req, res) => {
 	//Finds articles where saved is true
 	Article.find({ 'saved' : true }, (err, doc) => {
 		if (err) {
 			console.log(err);
 		}  else {
+			//Renders saved articles handlebars
 			res.render('articles-saved', {
 				articles: doc
 			});
@@ -145,24 +174,19 @@ app.get('/saved', (req, res) => {
 
 //Setting route to update an article to saved = true if user clicks "save article"
 app.post('/saved/:id', (req, res) => {
-	Article.update({ '_id' : req.params.id }, { $set : { 'saved' : true }}, (err, res) => {
-		if (err) {
-			console.log(err);
-		} else {
-			console.log(res);
-		}
-	});
+	Article.update({ '_id' : req.params.id }, { $set : { 'saved' : true }}, (err, res) => (err) ? console.log(err) : console.log(res)); 
 	res.redirect('/articles');
 })
 
 //Unsaves article
 app.post('/unsaved/:id', (req, res) => {
-	Article.update( { '_id' : req.params.id }, { $set : { 'saved' : false }}, (err, res) => 
-		(err) ? console.log(err) : console.log(res));
+	//Updates article with parametre _id by setting parameter saved to false
+	Article.update( { '_id' : req.params.id }, { $set : { 'saved' : false }}, (err, res) => (err) ? console.log(err) : console.log(res));
+	//Redirects to saved articles
 	res.redirect('/saved');
 });
 
 //Listening to the port 3000 or the environment PORT
-app.listen(process.env.PORT || 3000, function () {
+app.listen(process.env.PORT || 3000, () => {
 	console.log('App running on port 3000');
 });
